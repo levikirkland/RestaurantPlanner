7080967f099d13d600a72159f4e81a9b89b6161f
Andy Kirkland <levikirkland@outlook.com>
2022-04-07 00:36:52 -0400

    removed service dependency

--- RestaurantPlanner/Program.cs
+++ RestaurantPlanner/Program.cs
@@ -7,8 +7,12 @@ using RestaurantPlanner.Apis;
 using Microsoft.OpenApi.Models;
 using FluentValidation.AspNetCore;
 using System.Reflection;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 builder.Services.RegisterRepos();
 builder.Services.RegisterAuth(builder.Configuration);
