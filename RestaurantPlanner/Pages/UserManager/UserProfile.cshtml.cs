2c2544a5743f9ea9abf84a75d45fdcd4e5909fe0
Andy Kirkland <levikirkland@outlook.com>
2025-03-26 11:37:48 -0400

    pushing changes

---
+++ RestaurantPlanner/Pages/UserManager/UserProfile.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestaurantPlanner.Pages.UserManager;
+
+public class UserProfile : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
